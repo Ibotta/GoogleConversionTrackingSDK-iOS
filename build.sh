797eb0641a56766e21d51cb68bac55a51ca1e1eb
Scott Lemke <scott@ibotta.com>
2021-02-19 09:19:41 -0700

    Build an XCFramework

--- build.sh
+++ build.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-xcodebuild build -scheme GoogleConversionTrackingSDK -derivedDataPath derived_data -configuration Release BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild build \
+    -scheme GoogleConversionTrackingSDK \
+    -derivedDataPath derived_data \
+    -arch arm64 \
+    -sdk iphoneos BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+mkdir -p build/devices
+cp -r derived_data/Build/Products/Debug-iphoneos/ build/devices
+
+xcodebuild build \
+    -scheme GoogleConversionTrackingSDK \
+    -derivedDataPath derived_data \
+    -arch x86_64 \
+    -sdk iphonesimulator BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+mkdir -p build/simulators
+cp -r derived_data/Build/Products/Debug-iphonesimulator/ build/simulators
+
+xcodebuild -create-xcframework \
+    -framework build/simulators/GoogleConversionTrackingSDK.framework \
+    -framework build/devices/GoogleConversionTrackingSDK.framework \
+    -output build/GoogleConversionTrackingSDK.xcframework
 
 echo "Framwork location: derived_data/Build/Products/Release-iphoneos/GoogleConversionTrackingSDK.framework"
